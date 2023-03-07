54d60b078ecd9026cb63801c07c4799e232e148d
EldarMurzakhanov <eldarmurzahanov@gmail.com>
2023-03-07 05:42:10 +0300

    Added drag and drop test

---
+++ src/test/java/DragAndDropTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+
+    @Test
+    void DragAndDropCheck(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-b").$("header").shouldHave(text("B"));
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-b").$("header").shouldHave(text("A"));
+    }
+
+    @Test
+    void DragAndDropwithaction(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-b").$("header").shouldHave(text("B"));
+        $("#column-a").hover();
+        actions().clickAndHold().moveByOffset(250,0).release().perform();
+        $("#column-b").$("header").shouldHave(text("A"));
+
+    }
+}
